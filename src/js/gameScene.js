cb6dd26d84e760d68a62145e4772a92e66afd571
adrs2002 <adrs2002@hotmail.com>
2017-09-12 23:22:50 +0900

    commit

---
+++ src/js/gameScene.js
@@ -0,0 +1,15 @@
+/**
+ * 実際にゲームとして実装する際の、必要な処理を書き加える例がこちら
+ */
+class gameScene extends gameScene_base {
+
+  update(_delta) {
+    super._update(_delta);
+  }
+
+  draw() {
+    super._draw();
+  };
+
+
+}
