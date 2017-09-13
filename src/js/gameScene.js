ccbf90de049daf250cbe69aed41d6b7f2630cd3a
adrs2002 <adrs2002@hotmail.com>
2017-09-14 00:51:41 +0900

    add gameScene base System

--- src/js/gameScene.js
+++ src/js/gameScene.js
@@ -1,14 +1,14 @@
 /**
  * 実際にゲームとして実装する際の、必要な処理を書き加える例がこちら
  */
-class gameScene extends gameScene_base {
+class GameScene extends GameScene_base {
 
   update(_delta) {
-    super._update(_delta);
+   
   }
 
   draw() {
-    super._draw();
+   
   };
 
 
